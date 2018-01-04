a962c62cb804e0f6a1387397bc020ac6f7cb5052
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-01-03 16:02:26 -0800

    tag official binaries with git SHA

--- src/create-package.cmd
+++ src/create-package.cmd
@@ -1,5 +1,6 @@
-set PROJECT=.\IxMilia.ThreeMf\IxMilia.ThreeMf.csproj
+@echo off
+
+set PROJECT=%~dp0IxMilia.ThreeMf\IxMilia.ThreeMf.csproj
 dotnet restore %PROJECT%
 if errorlevel 1 exit /b 1
-dotnet pack --include-symbols --include-source --configuration Release %PROJECT%
-
+dotnet pack --include-symbols --include-source --configuration Release %PROJECT% /p:OfficialBuild=true
--- src/create-package.sh
+++ src/create-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh -e
 
+_SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
 PROJECT=./IxMilia.ThreeMf/IxMilia.ThreeMf.csproj
 dotnet restore $PROJECT
-dotnet pack --include-symbols --include-source --configuration Release $PROJECT
-
+dotnet pack --include-symbols --include-source --configuration Release $PROJECT /p:OfficialBuild=true
