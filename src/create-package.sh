c8f8e510d4d2d2ee88103f5d0f8fa9b37f8fa488
Brett V. Forsgren <brett.forsgren@outlook.com>
2018-11-30 09:43:46 -0800

    build everything to `artifacts/` with parameterized version

--- src/create-package.sh
+++ src/create-package.sh
@@ -1,6 +1,10 @@
 #!/bin/sh -e
 
 _SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
-PROJECT=./IxMilia.ThreeMf/IxMilia.ThreeMf.csproj
-dotnet restore $PROJECT
-dotnet pack --configuration Release $PROJECT
+PROJECT_NAME=IxMilia.ThreeMf
+CONFIGURATION=Release
+PROJECT=$_SCRIPT_DIR/$PROJECT_NAME/$PROJECT_NAME.csproj
+
+dotnet restore "$PROJECT"
+dotnet build "$PROJECT" --configuration $CONFIGURATION
+dotnet pack --no-restore --no-build --configuration $CONFIGURATION "$PROJECT"
